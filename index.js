df31a8d000b5f14d97992f5d856c7b1f1fb86e03
Вершинин Никита <vershinin.nekita@yandex.ru>
2020-03-23 01:02:10 +0500

    final

--- index.js
+++ index.js
@@ -13,11 +13,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
 
+const path = require('path');
+app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
-
-app.get('/*', (req, res) => {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+app.use('/api', userRouter);
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
