a2b8587463cfd694bcece45e4dabbc882a78aea4
Вершинин Никита <vershinin.nekita@yandex.ru>
2020-03-22 23:16:24 +0500

    new

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const crypto = require('crypto');
+const api = require('./controllers/session.js');
+const db = require('./data/db');
+const userRouter = require('./routes/router');
+
+const app = express();
+const apiPort = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.use('/api', userRouter);
+
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
