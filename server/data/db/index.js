8c258f01d59eb1ce5a548174dbf259875209d486
Вершинин Никита <vershinin.nekita@yandex.ru>
2020-03-28 20:54:38 +0500

    db

--- server/data/db/index.js
+++ server/data/db/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-const mongoUri = process.env.MONGODB_URI;
+
+const env = require('./env/environment');
+const mongoUri = `mongodb://${env.accountName}:${env.key}@${env.accountName}.documents.azure.com:${env.port}/${env.databaseName}?ssl=true`;
 
 const db = mongoose.connect(mongoUri, { useMongoClient: true })
     .then (() => console.log('MongoDB connected'))
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
-const db = require('./data/db');
+const db = require('./data/db/index');
 const userRouter = require('./routes/router');
 const cors = require('cors');
 
