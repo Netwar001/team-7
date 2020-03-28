bc53dde33fa7079fa6c4689efa73a8d9542839eb
Вершинин Никита <vershinin.nekita@yandex.ru>
2020-03-28 18:48:05 +0500

    env

--- server/data/db/index.js
+++ server/data/db/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const url = "mongodb+srv://Netwar:4VoVcSZ8qshOOSWT@cluster0-noog9.mongodb.net/users?retryWrites=true&w=majority";
+const url = process.env.MONGODB_URI || "mongodb+srv://Netwar:4VoVcSZ8qshOOSWT@cluster0-noog9.mongodb.net/users?retryWrites=true&w=majority";
 
 const db = mongoose.connect(url, {
     useNewUrlParser: true,
