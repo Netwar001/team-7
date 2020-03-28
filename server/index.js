5475f21cfd6d2f9af4d2e35abcefcb9a95dc6f24
Вершинин Никита <vershinin.nekita@yandex.ru>
2020-03-28 17:07:12 +0500

    index.js

--- server/index.js
+++ server/index.js
@@ -12,9 +12,9 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const path = require('path');
-app.use(express.static(path.join(__dirname, './client')));
+app.use(express.static(path.join(__dirname, 'client')));
 app.get('/*', function (req, res) {
-    res.sendFile(path.resolve('./client/index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
 
 app.use('/api', userRouter);
