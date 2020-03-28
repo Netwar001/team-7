5d83ce3befcc3dd1a4d08bd498261082b2bbe98a
Вершинин Никита <vershinin.nekita@yandex.ru>
2020-03-28 18:41:08 +0500

    Update index.js

--- server/data/db/index.js
+++ server/data/db/index.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
-const url = "mongodb://mongo-data:MDcfs7xKwbP2FEnB67cJTENOgoy61wrEG6tAQBWKOti6WWY3hjMObYPyo3ATfLUVX9LJz9zui38tTIYKkpk1wg%3D%3D@mongo-data.mongo.cosmos.azure.com:10255/?ssl=true&appName=@mongo-data@";
+const url = "mongodb+srv://Netwar:4VoVcSZ8qshOOSWT@cluster0-noog9.mongodb.net/users?retryWrites=true&w=majority";
 
 const db = mongoose.connect(url, {
     useNewUrlParser: true,
-	useMongoClient: true,
 })
     .then (() => console.log('MongoDB connected'))
     .catch((err) => console.log(err));
