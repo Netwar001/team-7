7f6adc15ebda393c03e5a6fb94e7037c72667070
Вершинин Никита <vershinin.nekita@yandex.ru>
2020-03-28 19:27:04 +0500

    Update index.js

--- server/data/db/index.js
+++ server/data/db/index.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const mongoUri = 'mongodb://mongo-data:MDcfs7xKwbP2FEnB67cJTENOgoy61wrEG6tAQBWKOti6WWY3hjMObYPyo3ATfLUVX9LJz9zui38tTIYKkpk1wg==@mongo-data.mongo.cosmos.azure.com:10255/users?ssl=true';
 
-const db = mongoose.connect("mongodb://mongo-data:MDcfs7xKwbP2FEnB67cJTENOgoy61wrEG6tAQBWKOti6WWY3hjMObYPyo3ATfLUVX9LJz9zui38tTIYKkpk1wg==@mongo-data.mongo.cosmos.azure.com:10255/[users]?ssl=true", {
-    useNewUrlParser: true,
-})
+const db = mongoose.connect(mongoUri, { useMongoClient: true })
     .then (() => console.log('MongoDB connected'))
     .catch((err) => console.log(err));
 
