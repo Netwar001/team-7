e24e1a7e60a3f984186dd366160e8e1e504a5585
Вершинин Никита <vershinin.nekita@yandex.ru>
2020-03-29 15:31:05 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,18 @@ const userRouter = require('./routes/router');
 const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI);
+//mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.Promise = global.Promise;
+
+const mongoUri = `mongodb://${process.env.COSMOSDB_ACCOUNT}:${process.env.COSMOSDB_KEY}@${
+  process.env.COSMOSDB_ACCOUNT
+}.mongo-data.mongo.cosmos.azure.com:${process.env.COSMOSDB_PORT}/${process.env.COSMOSDB_DB}?ssl=true`;
+
+function connect() {
+  mongoose.set('debug', true);
+  return mongoose.connect(mongoUri, { useMongoClient: true });
+}
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
