11c711435621b6d71e52b4a0d2ed351e4aea1a48
Вершинин Никита <vershinin.nekita@yandex.ru>
2020-03-29 10:47:58 +0500

    env

--- server/data/db/index.js
+++ server/data/db/index.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const env = require('./env/environment');
-const mongoUri = `mongodb://${env.accountName}:${env.key}@${env.accountName}.documents.azure.com:${env.port}/${env.databaseName}?ssl=true`;
+const mongoUri = process.env.MONGO_DB;
 
 function connect() {
 	mongoose.set('debug', true);
