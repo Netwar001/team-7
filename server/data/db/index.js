854a40c0e56aa3fb9aec25f30bbe0f9e1cdc45ef
Вершинин Никита <vershinin.nekita@yandex.ru>
2020-03-29 15:03:54 +0500

    Update index.js

--- server/data/db/index.js
+++ server/data/db/index.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const mongoUri = process.env.MONGODB_URI;
-
 function connect() {
-	mongoose.set('debug', true);
-	return mongoose.connect(mongoUri, { useMongoClient: true });
+	return mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 }
 
 module.exports = {
