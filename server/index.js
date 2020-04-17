525c10d29f58f7ce4baabe42cc42c8f845c0ff4f
Вершинин Никита <vershinin.nekita@yandex.ru>
2020-04-17 22:47:35 +0500

    sessions

--- server/index.js
+++ server/index.js
@@ -1,21 +1,2 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const userRouter = require('./routes/router')
-const cors = require('cors')
-const app = express()
-const db = require('./data/db')
-const session = require('./controllers/session')
-
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-app.use(bodyParser.json())
-
-const path = require('path')
-app.use(express.static(path.join(__dirname, 'client')))
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client', 'index.html'))
-})
-
-app.use('/api', userRouter)
-
-app.listen(process.env.PORT || 3000)
+require = require("esm")(module/*, options*/)
+module.exports = require("./server.js")
